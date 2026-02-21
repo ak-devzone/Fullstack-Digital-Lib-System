c2293fdd2e12fce7dc02a490f73ffa2eb03a1f2e
BuildWithAK <contact.buildwithak@gmail.com>
2026-02-21 22:24:28 +0530

    ch

--- backend/library_system/settings.py
+++ backend/library_system/settings.py
@@ -33,8 +33,10 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-your-secret-key-here-chang
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
+DEBUG = False
+
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',') + [
-    'evelia-umbrose-unmovingly.ngrok-free.dev',
+    'evelia-umbrose-unmovingly.ngrok-free.dev', '.vercel.app', '.vercel.pub',
 ]
 
 # Application definition
